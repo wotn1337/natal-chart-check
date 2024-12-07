4bf93ea30857ae12d8462a9f21c5518373043687
Артем Белов <theflash.0320@gmail.com>
2024-12-07 20:05:04 +0500

    add dotenv config

--- constants.js
+++ constants.js
@@ -1,4 +1,5 @@
-import "dotenv/config";
+import dotenv from "dotenv";
+dotenv.config({ path: ".env" });
 
 export const WEBSITE_URL = process.env.WEBSITE_URL;
 export const SHOT_DATE_TITLE = "СЪЁМКИ НОВЫХ ВЫПУСКОВ";
