2b822063af9e6cf0605efdcac32ad3f1ff7b992c
Артем Белов <theflash.0320@gmail.com>
2024-12-08 19:06:28 +0500

    delete clock checker

--- constants.js
+++ constants.js
@@ -11,9 +11,6 @@ export const WEBSITE_URL = process.env.WEBSITE_URL;
 export const SHOT_DATE_TITLE = "СЪЁМКИ НОВЫХ ВЫПУСКОВ";
 export const OLD_DATE_STRING = process.env.OLD_DATE_STRING;
 
-export const BORDER_TIME = process.env.BORDER_TIME;
-export const TIME_WEBSITE_URL = process.env.TIME_WEBSITE_URL;
-
 export const EMAIL_CONFIG = {
   host: process.env.EMAIL_HOST,
   port: process.env.EMAIL_PORT,
